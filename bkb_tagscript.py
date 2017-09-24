624f22a12852a705f6363cb3eebf9598f549b9ec
eskolar <eskolar@eskolar.com>
2017-09-24 23:12:55 +0200

    colours svg.ok

--- bkb_tagscript.py
+++ bkb_tagscript.py
@@ -58,7 +58,7 @@ tcolor   = ['white', 'red',     'black',    '#FF0000',  '#000080',
             'orange', 'orange', 'blue',     'blue',     'red']
 
 # bkb added 2x 0.1 for blue
-topacity = [ 0.3,     0.6,        0.9,        1,        1,     
+topacity = [ 0.3,     1,          0.9,        0.8,        1,     
              0.6 ,    0.8,        0.8,        0.3,      0.5,      
              0.8,     0.4,        0.3,        0.1,      0.1  ]
 
